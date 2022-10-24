99883ab4c1b2f157f72dba7a5f17485ed1479209
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-10-24 15:45:17 +0200

    [add]function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-/*   DATI  */
 const arrayImg = [
     '01.jpg',
     '02.jpg',
@@ -10,10 +9,6 @@ const arrayImg = [
 // "CHIAMO" I MIEI CONTENITORI DELLE IMG
 const containerImgLg = document.querySelector('.img-lg');
 const containerImgXs = document.querySelector('.img-xs');
-
-// CREO DUE COSTANTI CON I MIEI BOTTONI PER SCROLLARE LE IMMAGINI
-const up = document.querySelector('.btn.up');
-const down = document.querySelector('.btn.down');
   
 // CREO LE COSTANTI CHE POI USERO' PER INTERAGIRE CON L'HTML
 let imgLg = "";
@@ -22,10 +17,10 @@ let imgXs = "";
 // CREO IL CONTATORE PER LA GESTIONE DELLE CLASSI
 let counterImg = 0;
   
-  
+ 
 // "SCOMPONGO" L'ARRAY USANDO IL CICLO FOR E STAMPO I TAG CHE MI SERVONO
 for(let i = 0; i < arrayImg.length; i++){
-    //inseriamo le immagini nel box xl
+    //CREO IL TAG IMG PER IL BOX XL
     imgLg = `
     <img class="xl hide" src="img/${arrayImg[i]}" alt="${arrayImg[i]}">
     `;
@@ -39,6 +34,9 @@ for(let i = 0; i < arrayImg.length; i++){
     containerImgXs.innerHTML += imgXs;
 }
   
+// PRENDO IL COINTAINER CHE UTILIZZERO' PER L'OVER
+const carousel = document.querySelector('.carousel');
+  
 // CREO LA SITUAZIONE DI DEFAULT CHE MI OCCORRE:
 // PRENDO I CONTENITORI CHE MI SERVIRANNO
 const arrayImgLg = document.getElementsByClassName('xl');
@@ -50,39 +48,47 @@ arrayImgLg[counterImg].classList.add('active');
   
 arrayImgXs[counterImg].classList.add('active');
   
+// CREO DUE COSTANTI CON I MIEI BOTTONI PER SCROLLARE LE IMMAGINI
+const up = document.querySelector('.btn.up');
+const down = document.querySelector('.btn.down');
+
+// do un intervallo per il cambio immagini e una variabile globale
+let changeTime = 1.7 ;
+let startInterval;
+  
+// loop che cambia le immagini
+startInterval = setInterval(function(){
+  upDown(true);
+}, changeTime * 1000)
 
   
+down.addEventListener('click',function(){
+    upDown(true);
+})
   
-// forma compatta per dare  la funzione ai bottoni
-down.addEventListener('click', function(){
+up.addEventListener('click', function (){
+    upDown(false);
+})
+  
+  
+  
+function upDown(isDown){
     arrayImgLg[counterImg].classList.remove('active');
     arrayImgLg[counterImg].classList.add('hide');
   
     arrayImgXs[counterImg].classList.remove('active');
   
-    counterImg++;
-    if(counterImg === arrayImg.length) counterImg = 0;
+    if(isDown){
+      counterImg++;
+      if(counterImg === arrayImg.length) counterImg = 0;
+    }else {
+      counterImg--;
+      if(counterImg < 0) counterImg = arrayImg.length -1;
+    }
   
     arrayImgLg[counterImg].classList.remove('hide');
     arrayImgLg[counterImg].classList.add('active');
   
     arrayImgXs[counterImg].classList.add('active')
+}
   
-})
-
-  
-up.addEventListener('click', function(){
-    arrayImgLg[counterImg].classList.remove('active');
-    arrayImgLg[counterImg].classList.add('hide');
-  
-    arrayImgXs[counterImg].classList.remove('active');
-  
-    counterImg--;
-    if(counterImg < 0) counterImg = arrayImg.length -1;
-  
-    arrayImgLg[counterImg].classList.remove('hide');
-    arrayImgLg[counterImg].classList.add('active');
-  
-    arrayImgXs[counterImg].classList.add('active')
-  
-})
